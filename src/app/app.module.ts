8585a00aa8a4ae89bdbdebb729cb0cfba09c2fd6
Lucatorze <lucas.roberts@laposte.net>
2018-10-05 16:57:10 +0200

    FirstTest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
